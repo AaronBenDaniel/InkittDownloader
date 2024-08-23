9ddadf146ed32233c48c19f20924aa12edceffcc
AaronBenDaniel <144371000+AaronBenDaniel@users.noreply.github.com>
2024-08-23 14:32:32 -0400

    Revert "added some padding to the input boxes"
    This reverts commit a604c4a879c2691104dbeb514bf56c3f11d4e9d3.

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -55,40 +55,36 @@
         <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form class="card-body">
             <div class="form-control">
-              <span>
+              <input
+                type="text"
+                placeholder="Story URL"
+                class="input input-bordered"
+                bind:value={raw_story_id}
+                required
+                name="story_id"
+              />
+              <label class="input input-bordered flex items-center gap-2">
+                Username
                 <input
                   type="text"
-                  placeholder="Story URL"
-                  class="input input-bordered"
-                  bind:value={raw_story_id}
+                  class="grow"
+                  name="username"
+                  placeholder="foxtail.chicken"
+                  bind:value={credentials.username}
                   required
-                  name="story_id"
-                /></span
-              ><span>
-                <label class="input input-bordered flex items-center gap-2">
-                  Username
-                  <input
-                    type="text"
-                    class="grow"
-                    name="username"
-                    placeholder="foxtail.chicken"
-                    bind:value={credentials.username}
-                    required
-                  />
-                </label></span
-              ><span>
-                <label class="input input-bordered flex items-center gap-2">
-                  Password
-                  <input
-                    type="password"
-                    class="grow"
-                    placeholder="supersecretpassword"
-                    name="password"
-                    bind:value={credentials.password}
-                    required
-                  />
-                </label></span
-              >
+                />
+              </label>
+              <label class="input input-bordered flex items-center gap-2">
+                Password
+                <input
+                  type="password"
+                  class="grow"
+                  placeholder="supersecretpassword"
+                  name="password"
+                  bind:value={credentials.password}
+                  required
+                />
+              </label>
             </div>
 
             <div class="form-control mt-6">
