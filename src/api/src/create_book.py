26fdae632f33390ab750f223ef8aa0d2e7d74ab5
AaronBenDaniel <144371000+AaronBenDaniel@users.noreply.github.com>
2024-08-23 10:57:46 -0400

    I'll figure out tags later

--- src/api/src/create_book.py
+++ src/api/src/create_book.py
@@ -161,9 +161,9 @@ def set_metadata(book, data):
     #book.add_metadata("DC", "modified", data["modifyDate"])
     book.add_metadata("DC", "language", data["language"]["locale"])
 
-    book.add_metadata(
-        None, "meta", "", {"name": "tags", "content": ", ".join(data["content_labels"])}
-    )
+    # book.add_metadata(
+    #     None, "meta", "", {"name": "tags", "content": ", ".join(data["content_labels"])}
+    # )
     # book.add_metadata(
     #     None, "meta", "", {"name": "mature", "content": str(int(data["mature"]))}
     # )
